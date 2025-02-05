941bf2c74bda7e1fe96ca262ed59863687c4a8b0
NatalieR2026 <natalie.radin2026@gbwl.org>
2025-02-05 14:42:48 +0000

    2/5

--- Main.java
+++ Main.java
@@ -72,8 +72,31 @@ public class Main {
       toBuy.remove(3);
       System.out.println(toBuy);
 
-      System.out.println("---");
+      // 7.3 Traversing with Loops
+      ArrayList<Integer> bingoNumbers = new ArrayList<Integer>();
+      bingoNumbers.add(99);
+      bingoNumbers.add(29);
+      bingoNumbers.add(77);
+      bingoNumbers.add(13);
 
+      // FOR-EACH LOOP
+      int total = 0;
+      int product = 1;
+      for (Integer num : bingoNumbers) {
+         total = total + num; // same as total += num
+         product *= num; // same as product = product * num
+      }
+      System.out.println("Sum of bingo numbers: " + total);
+      System.out.println("Product of bingo numbers: " + product);
+
+
+
+
+
+
+
+
+      /*
       // PRACTICE: Translate Array actions to ArrayList actions
       ArrayList<String> toDoList = new ArrayList<String>();
       toDoList.add("Do homework");
@@ -86,9 +109,7 @@ public class Main {
       System.out.println("First thing to do: " + toDoList.get(0));
       toDoList.remove(0);
       System.out.println(toDoList);
-
-
-
+      */
 
    }
 }
